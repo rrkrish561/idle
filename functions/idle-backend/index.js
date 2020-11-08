36d5cae251db639238593e8f93d9087c8798b45b
Raghu Radhakrishnan <rradhakrishnan1@ufl.edu>
2020-11-07 22:16:11 -0500

    fix thing

--- functions/idle-backend/index.js
+++ functions/idle-backend/index.js
@@ -21,20 +21,36 @@ exports.detectFace = (req, res) => {
         console.log(req.body)
 
         // res.status(200).send(req.body)
-        detect().then((result) => {
-            console.log(img)
-            res.status(200).send(result);
+        detect(req.body.base64).then((result) => {
+            res.status(200).send(JSON.stringify(result));
         }).catch((err) => {
             console.log(err)
+            res.status(400).send(JSON.stringify(err))
         })
     } 
 };
 
-async function detect(img) {
+async function detect(base64Img) {
     // Creates a client
     const client = new vision.ImageAnnotatorClient();
 
-    const [result] = await client.faceDetection(img);
+    //const img = fs.readFileSync(base64Img)
+    //console.log(img)
+
+    //var encoded = Buffer.from(img).toString('base64');
+    //console.log(encoded)
+
+    const request = {
+        image: {
+            content: base64Img
+        },
+        features: [
+            {
+                type: 'FACE_DETECTION'
+            }
+        ]
+    }
+    const [result] = await client.annotateImage(request);
 
     return result
 
@@ -50,3 +66,5 @@ async function detect(img) {
     //     console.log(`    Surprise: ${face.surpriseLikelihood}`);
     // });
 }
+
+//detect('./images/hi.jpeg').then((result) => { console.log(result) }).catch((err) => { console.log(err) })
