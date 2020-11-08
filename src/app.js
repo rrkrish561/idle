5c09a2180238630c5d58074384c2b5e7e0be8260
Nitin Ramadoss <nitinramadoss@gmail.com>
2020-11-07 20:22:00 -0500

    conversion to b64

--- src/app.js
+++ src/app.js
@@ -17,18 +17,29 @@ function initiateCamera() {
         console.log(err);
     });
 
-    setTimeout(function(){processImage(webcam)}, 3000);
+    setTimeout(function(){processImage(webcam, canvas)}, 3000);
 } 
 
-function processImage(webcam) {
-    let base64Image = webcam.snap();
-    
-    const image = {base64: base64Image};
+function processImage(webcam, canvas) {
+    let ctx = canvas.getContext('2d');
+    let png = webcam.snap();
+
+    let imageObj = new Image();
+    imageObj.src = png;
+
+    ctx.drawImage(imageObj, 0, 0);
+
+    let uri = canvas.toDataURL('image/png'),
+    b64 = uri.replace(/^data:image.+;base64,/, '');
+
+    const image = {base64: b64};
+
     console.log(image)
+
     let url = 'https://cors-anywhere.herokuapp.com/https://us-central1-micro-access-294918.cloudfunctions.net/detectFace';
 
     fetch(url, {headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, method: "POST", body: JSON.stringify(image)})
-        .then(response => response.json())
+        .then(response => JSON.parse(response))
         .then(data => console.log(data));
 }
 
